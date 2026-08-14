9737d1447c333cb49255bfb2e511b62f30f6ee16
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-08-14 21:35:56 +0530

    updated

--- patients/models.py
+++ patients/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class Treatment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Patient(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     phone = models.CharField(max_length=15)
     disease = models.TextField()
+
+    treatments = models.ManyToManyField(Treatment, blank=True)
 
     def __str__(self):
         return self.name
