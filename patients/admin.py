75c465622937aff6c00764a1f85239b769b8f53e
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-08-15 08:15:43 +0530

    updated

--- patients/admin.py
+++ patients/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Patient, MedicalRecord
+from .models import Patient, MedicalRecord, Treatment, Profile
 
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'bio')
+    
+@admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age', 'phone', 'disease')
+    list_display = (
+        'name',
+        'age',
+        'phone',
+        'disease',
+        'show_treatments'
+    )
     search_fields = ('name', 'phone')
     list_filter = ('disease',)
     list_per_page = 10
 
-admin.site.register(Patient)
-admin.site.register(MedicalRecord)
+    def show_treatments(self, obj):
+        return ", ".join(
+            treatment.name
+            for treatment in obj.treatments.all()
+        )
+
+    show_treatments.short_description = 'Treatments'
--- patients/models.py
+++ patients/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True
+    )
+    bio = models.TextField(
+        blank=True
+    )
+    phone = models.CharField(
+        max_length=15,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
 
 class Treatment(models.Model):
     name = models.CharField(max_length=100)
